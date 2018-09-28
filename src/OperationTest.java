2dc2701b5c20abf31a6431fcb5009a9f5070d136
sam bauer <42975601+samuelb1130@users.noreply.github.com>
2018-09-28 12:41:11 -0400

    Add files via upload

---
+++ src/OperationTest.java
@@ -0,0 +1,21 @@
+
+public class OperationTest
+{
+
+	public static void main(String[] args) 
+	{
+		int number; //Declaration statement
+		number  = 20; //Assigment statement -> initialzation
+		number = number += 5; // number+= 5
+		System.out.println(number);
+		number /= 2;
+		System.out.println(number);
+		number -= 6;
+		System.out.println(number);
+		number *= 4;
+		System.out.println(number);
+		number %= 2;
+		System.out.println(number);
+	}
+
+}	
